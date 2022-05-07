b11272dcabff9f06550e6c6f39d8d597e1f799c4
Deividhy Tonetti <deividhytonetti@gmail.com>
2022-05-06 23:29:04 -0300

    [Fix]: Change async routes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.post("/create/")
-def create_card(
+async def create_card(
     name: str,
     edition: str,
     language: str,
@@ -16,15 +16,15 @@ def create_card(
     return 0
 
 @app.get("/listCard/{name_card}")
-def list_card_by_name(name_card: str):
+async def list_card_by_name(name_card: str):
     return {"item_id": name_card}
 
 @app.get("/listAllCards/")
-def list_all_cards():
+async def list_all_cards():
     return {"item_id"}
 
 @app.put("/editCard/{name_card}")
-def update_card(
+async def update_card(
     name: str,
     edition: str,
     language: str,
@@ -36,5 +36,5 @@ def update_card(
     return {"item_name"}
 
 @app.delete("/deleteCard/{name_card}")
-def list_card_by_name(name_card: str):
+async def list_card_by_name(name_card: str):
     return 0
