7f1b85a50618dfc7a3ff61d0781a5b128513eb93
Deividhy Tonetti <deividhytonetti@gmail.com>
2022-05-08 10:44:37 -0300

    [Feat]: Inclusão de informações na main

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
 from Routes.router import router
+from config.db import engine
 
-app = FastAPI()
+app = FastAPI(
+    title='Magic The Gathering',
+    description='Crud simples de game de cartas :)',
+    version='0.0.1',
+    contact={
+        'Name': 'Deividhy J. Tonetti',
+        'Email': 'deividhytonetti@hotmail.com',
+        'whats': '(+55) (12) 9 8296-7213 '
+    }
+)
+
 app.include_router(router, prefix='')
